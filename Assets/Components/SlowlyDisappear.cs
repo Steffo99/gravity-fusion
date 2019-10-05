005229e2c33892ba23336f6e65b3a16c4f2fccec
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-10-05 13:13:26 +0200

    Add SlowlyDisappear component

---
+++ Assets/Components/SlowlyDisappear.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowlyDisappear : MonoBehaviour
+{
+    public float disappearAfter;
+    protected float timeLeft;
+    protected SpriteRenderer sprite;
+
+    protected float FractionLeft {
+        get {
+            return timeLeft / disappearAfter;
+        }
+    }
+
+    private void Start() {
+        sprite = GetComponent<SpriteRenderer>();
+        timeLeft = disappearAfter;
+    }
+
+    private void Update() {
+        timeLeft -= Time.deltaTime;
+        
+        if(sprite != null) {
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, FractionLeft);  
+        }
+
+        if(timeLeft < 0) {
+            Destroy(this.gameObject);
+        }
+    }
+}
