bd75ae9524bf2534b3b7bd09ea5f1de9ef830301
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-10-05 13:06:48 +0200

    Merge into suns

--- Assets/Components/Merge.cs
+++ Assets/Components/Merge.cs
@@ -7,6 +7,7 @@ public class Merge : MonoBehaviour
 {
     [Header("Config")]
     public GameObject mergeIntoPrefab;
+    public bool setMergedMassFromTotal;
 
     [Header("References")]
     protected new CircleCollider2D collider;
@@ -23,30 +24,42 @@ public class Merge : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other) {
         Merge otherMerge = other.GetComponent<Merge>();
-        mergeables.Add(otherMerge);
-
-        if(CanMerge()) DoMerge();
+        if(otherMerge != null) {
+            mergeables.Add(otherMerge);
+            if(CanMerge()) DoMerge();
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
         Merge otherMerge = other.GetComponent<Merge>();
-        mergeables.Remove(otherMerge);
+        if(otherMerge != null) {
+            mergeables.Remove(otherMerge);
+        }
     }
 
     protected bool CanMerge() {
-        return false;
+        return mergeables.Count >= 5;
     }
 
     protected void DoMerge() {
         GameObject mergeResult = Instantiate(mergeIntoPrefab, transform.position, Quaternion.identity);
         MergedInfo mergedInfo = mergeResult.AddComponent<MergedInfo>();
         mergedInfo.mergedCount = mergeables.Count;
-        foreach(Merge merged in mergeables) {
+
+        foreach(Merge merged in mergeables.ToArray()) {
             Rigidbody2D mergedBody = merged.GetComponentInParent<Rigidbody2D>();
             if(mergedBody != null) {
                 mergedInfo.mergedMass += mergedBody.mass;
             }
-            Destroy(merged.transform.parent);
+            Destroy(merged.transform.parent.gameObject);
+        }
+        
+        if(setMergedMassFromTotal) {
+            Rigidbody2D mergeRigidbody = mergeResult.GetComponent<Rigidbody2D>();
+            if(mergeRigidbody != null) {
+                mergeRigidbody.useAutoMass = false;
+                mergeRigidbody.mass = mergedInfo.mergedMass;
+            }
         }
     }
 }
