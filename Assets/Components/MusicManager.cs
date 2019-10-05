1f86bb26fd06cc9d3f975fcfdf9a3c782b479662
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-10-05 15:59:17 +0200

    Add Music Manager

---
+++ Assets/Components/MusicManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[Serializable]
+public class MusicLayer {
+    public AudioSource audioSource;
+
+    public MusicLayer(AudioSource audioSource) {
+        this.audioSource = audioSource;
+    }
+
+    public float Volume {
+        get {
+            return audioSource.volume;
+        }
+        set {
+            audioSource.volume = value;
+        }
+    }
+}
+
+
+public class MusicManager : MonoBehaviour
+{
+    public List<MusicLayer> layers;
+
+    void Start()
+    {
+        List<AudioSource> audioSources = new List<AudioSource>();
+        GetComponentsInChildren<AudioSource>(true, audioSources);
+        foreach(AudioSource audioSource in audioSources) {
+            MusicLayer layer = new MusicLayer(audioSource);
+            layer.Volume = 0;
+        }
+    }
+}
