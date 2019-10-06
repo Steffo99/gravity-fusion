cf4e9fd0921ac56764dc19a9666aed74e7651e75
Stefano Pigozzi <ste.pigozzi@gmail.com>
2019-10-07 00:45:52 +0200

    Add TempUpgradesToggler

---
+++ Assets/Components/TempUpgradesToggler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempUpgradesToggler : MonoBehaviour
+{
+    public GameObject upgrades;
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P)) {
+            upgrades.SetActive(!upgrades.activeSelf);
+        }
+    }
+}
